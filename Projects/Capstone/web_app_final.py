8652e5aef45dde3860409746c8df1db3608b18a0
LouisMcArdell <mcardeloui@gmail.com>
2024-02-06 17:16:30 +1300

    Tuesday work 06.02.2024

--- Projects/Capstone/web_app_final.py
+++ Projects/Capstone/web_app_final.py
@@ -5,6 +5,10 @@ from sklearn.cluster import DBSCAN
 import folium
 from streamlit_folium import st_folium
 
+
+# Set Streamlit page configuration to use wide mode
+st.set_page_config(layout='wide', page_title='NZ Crash Data Clustering')
+
 # Custom CSS to enable scrolling in the sidebar (or any container)
 st.markdown("""
 <style>
@@ -32,6 +36,9 @@ if "center" not in st.session_state:
         st.session_state["center"] = CENTER_START
 if "zoom" not in st.session_state:
         st.session_state["zoom"] = ZOOM_START
+
+if 'run_clustering' not in st.session_state:
+    st.session_state['run_clustering'] = False        
    
 
 @st.cache_data
@@ -58,9 +65,27 @@ def plot_on_map(clustered_df):
         'Serious Crash': '#d62728', 
         'Fatal Crash': '#4d4d4d'
     }
+
     map_center = [clustered_df['latitude'].mean(), clustered_df['longitude'].mean()]
-    map = folium.Map(location=map_center, zoom_start=14)  # Adjust zoom_start for closer view
     
+    # Create the map with an initial tile layer
+    map = folium.Map(location=map_center, zoom_start=16, tiles=None)  # Setting tiles=None to start with no tiles
+    
+    # Define different tile layers including CartoDB Positron and Esri Satellite
+    tiles = [
+        ('Esri Satellite', 
+         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', 
+         {'attr': 'Esri', 'overlay': False, 'name': 'Esri Satellite'}),
+        ('CartoDB Positron', 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
+         {'attr': 'CartoDB', 'overlay': False, 'name': 'CartoDB Positron'}),
+    ]
+
+    # Add tile layers to the map
+    for name, tile_url, options in tiles:
+        folium.TileLayer(tile_url, attr=options['attr'], name=options['name'], overlay=options['overlay']).add_to(map)
+
+
+    # Add markers with tooltips as unique identifiers
     for idx, row in clustered_df.iterrows():
         folium.CircleMarker(
             location=[row['latitude'], row['longitude']],
@@ -68,23 +93,76 @@ def plot_on_map(clustered_df):
             color=severity_colors[row['crashSeverity']],
             fill=True,
             fill_color=severity_colors[row['crashSeverity']],
-            popup=f"Cluster: {row['cluster']}"
+            tooltip=str(idx),  # Use the DataFrame index or a unique identifier column as tooltip
         ).add_to(map)
     
+    # Add LayerControl to allow users to switch between layers
+    folium.LayerControl().add_to(map)
+
     return map
 
-def display_map(map_object):
+def prepare_cluster_summary(df):
+    # Count occurrences of each cluster
+    cluster_counts = df['cluster'].value_counts().rename_axis('Cluster').reset_index(name='Crash Count')
+    
+    # Find the most common 'region' and 'urban' value for each cluster
+    most_common_region = df.groupby('cluster')['region'].agg(lambda x: x.mode()[0] if not x.mode().empty else np.nan).rename_axis('Cluster').reset_index(name='Region')
+    most_common_urban = df.groupby('cluster')['urban'].agg(lambda x: x.mode()[0] if not x.mode().empty else np.nan).rename_axis('Cluster').reset_index(name='Urban/Open Road')
+    
+    # Merge the aggregated dataframes
+    summary_df = pd.merge(cluster_counts, most_common_region, on='Cluster')
+    summary_df = pd.merge(summary_df, most_common_urban, on='Cluster')
+
+    # Sort the summary DataFrame by 'Cluster' to ensure it is ordered by cluster number
+    summary_df.sort_values(by='Cluster', inplace=True)
+    
+    # Set 'Cluster' as the index of the DataFrame
+    summary_df.set_index('Cluster', inplace=True)
+    
+    return summary_df
+
+
+def display_transposed_summary(cluster_df, cluster_number):
     """
-    Displays a Folium map in Streamlit using st_folium.
+    Displays a transposed summary for the selected cluster.
 
     Parameters:
-    - map_object: A Folium map object to display.
+    - cluster_df: DataFrame containing data for the selected cluster.
+    - cluster_number: The number of the selected cluster.
     """
-    # Display the Folium map in Streamlit
-    st_folium(map_object, width=725, height=500)
+    # Aggregate data for the selected cluster
+    most_common_region = cluster_df['region'].mode()[0] if not cluster_df['region'].mode().empty else 'N/A'
+    most_common_urban = cluster_df['urban'].mode()[0] if not cluster_df['urban'].mode().empty else 'N/A'
+    most_common_speed_limit = cluster_df['speedLimit'].mode()[0] if not cluster_df['speedLimit'].mode().empty else 'N/A'
+    crash_count = len(cluster_df)
+    center_latitude = cluster_df['latitude'].mean()
+    center_longitude = cluster_df['longitude'].mean()
+
+    # Generate Google Maps URL with a marker at the center point
+    maps_url = f"https://www.google.com/maps?q={center_latitude},{center_longitude}&hl=en&z=17"
+
+    # Generate a direct Google Street View URL
+    street_view_url = f"http://maps.google.com/maps?q=&layer=c&cbll={center_latitude},{center_longitude}"
+
+    # Create a Markdown string for the transposed table
+    summary_md = f"""
+    | Field               | Value     |
+    |---------------------|-----------|
+    | Cluster Number      | {cluster_number} |
+    | Crash Count         | {crash_count} |
+    | Region  | {most_common_region} |
+    | Urban/Open Road     | {most_common_urban} |
+    | Speed Limit | {most_common_speed_limit} |
+    | View on Google Maps       | [Google Maps]({maps_url}) |
+    | View on Google Street View| [Street View]({street_view_url}) |   
+    """
+    
+    # Display the Markdown table in Streamlit
+    st.markdown(summary_md)
 
 # Streamlit app interface
-st.title('Crash Data Clustering')
+st.title('NZ Crash Data Clustering')
+st.subheader('Source: NZTA Crash Analysis System (CAS)')
 
 with st.sidebar:
     # Checkboxes for crash severity selection
@@ -99,27 +177,95 @@ with st.sidebar:
     run_clustering = st.button('Run Clustering')
 
 if run_clustering:
+    st.session_state['run_clustering'] = True
     filtered_df = df[df['crashSeverity'].isin(selected_severities)]
     if not filtered_df.empty:
         cluster_labels = perform_clustering(filtered_df, epsilon_km, min_samples)
         filtered_df['cluster'] = cluster_labels
-        # Update session state with filtered DataFrame excluding noise (-1 label)
         st.session_state['clustered_df'] = filtered_df[filtered_df['cluster'] != -1]
 
-# Main content area for displaying selected cluster map and summary
-if not st.session_state['clustered_df'].empty:
-    # Dropdown for selecting a cluster moved to sidebar to ensure selection can happen anytime
-    selected_cluster = st.sidebar.selectbox("Select a cluster to view on map:", st.session_state['clustered_df']['cluster'].unique(), key='selected_cluster')
+main_col, right_sidebar = st.columns([3, 3])
 
-    selected_cluster_df = st.session_state['clustered_df'][st.session_state['clustered_df']['cluster'] == selected_cluster]
-    result_map = plot_on_map(selected_cluster_df)
-    display_map(result_map)
+with main_col:
+    if st.session_state.get('run_clustering', False) and 'clustered_df' in st.session_state and st.session_state['clustered_df'] is not None and not st.session_state['clustered_df'].empty:
+        unique_clusters = np.sort(st.session_state['clustered_df']['cluster'].unique())
+        selected_cluster = st.sidebar.selectbox("Select a cluster to view on map:", unique_clusters, key='selected_cluster')
+        
+        if selected_cluster is not None:
+            selected_cluster_df = st.session_state['clustered_df'][st.session_state['clustered_df']['cluster'] == selected_cluster]
+            result_map = plot_on_map(selected_cluster_df)
+            map_response = st_folium(result_map, width=725, height=500)
 
-    # Display the cluster summary under the map
-    st.write("Cluster Summary")
-    st.table(st.session_state['clustered_df']['cluster'].value_counts().sort_index().rename_axis('Cluster').reset_index(name='Count'))
-else:
-    if run_clustering:
-        st.warning("No clusters found. Adjust parameters and try again.")
+            if map_response:
+                # Explicitly handle None values for last_clicked and last_object_clicked
+                last_clicked = map_response.get('last_clicked') or {}
+                last_object_clicked = map_response.get('last_object_clicked') or {}
+
+                # Now, safely check for 'lat' and 'lng' without causing TypeError
+                last_clicked_lat = round(last_clicked.get('lat', 0), 6)
+                last_clicked_lng = round(last_clicked.get('lng', 0), 6)
+                last_clicked_lat_lng = (last_clicked_lat, last_clicked_lng)
+
+                last_object_clicked_lat = round(last_object_clicked.get('lat', 0), 6)
+                last_object_clicked_lng = round(last_object_clicked.get('lng', 0), 6)
+                last_object_clicked_lat_lng = (last_object_clicked_lat, last_object_clicked_lng)
+
+                # Check if last clicked location is different from last object clicked location
+                if last_clicked_lat_lng != last_object_clicked_lat_lng:
+                    # Different location clicked; reset selected crash ID
+                    st.session_state['selected_crash_id'] = None
+                else:
+                    # Same location or a crash marker clicked; update selected crash ID if available
+                    last_clicked_tooltip = map_response.get('last_object_clicked_tooltip')
+                    st.session_state['selected_crash_id'] = last_clicked_tooltip if last_clicked_tooltip else st.session_state.get('selected_crash_id', None)
+            
+            # Prepare and display the summary for all clusters below the map
+            st.write("Overall Cluster Summary")
+            cluster_summary_df = prepare_cluster_summary(st.session_state['clustered_df'])
+            st.dataframe(cluster_summary_df)  # Displaying the DataFrame for all clusters
+
+with right_sidebar:
+
+    # Display Cluster Summary
+    if 'selected_cluster' in st.session_state and st.session_state['selected_cluster'] is not None:
+        st.write(f"Details for Cluster {st.session_state['selected_cluster']}:")
+
+        # Filter the DataFrame for the selected cluster
+        selected_cluster_df = st.session_state['clustered_df'][st.session_state['clustered_df']['cluster'] == st.session_state['selected_cluster']]
+
+        # Display cluster summary
+        if not selected_cluster_df.empty:
+            display_transposed_summary(selected_cluster_df, st.session_state['selected_cluster'])
+        else:
+            st.write("No details available for this cluster.")
+    
+    # Divider for visual separation
+    st.markdown("---")
+    
+
+    # Conditional display based on whether a crash ID has been selected
+    if 'selected_crash_id' in st.session_state and st.session_state['selected_crash_id']:
+        selected_crash_id = st.session_state['selected_crash_id']
+        
+        # Ensure selected_cluster is available and valid
+        if 'selected_cluster' in st.session_state and st.session_state['selected_cluster'] is not None:
+            # Recreate selected_cluster_df based on the current selected cluster
+            selected_cluster_df = st.session_state['clustered_df'][st.session_state['clustered_df']['cluster'] == st.session_state['selected_cluster']]
+
+            # Attempt to find the selected crash within the selected cluster's DataFrame
+            selected_crash_detail_df = selected_cluster_df[selected_cluster_df.index.astype(str) == selected_crash_id]
+
+            if not selected_crash_detail_df.empty:
+                st.write("Details for the selected crash:")
+                st.dataframe(selected_crash_detail_df)
+            else:
+                # Display a message when details for the selected crash are not found
+                st.write("No details found for the selected crash.")
     else:
-        st.write("Select severities and parameters, then click 'Run Clustering' to display results.")
+        # Display a placeholder message when no crash is selected
+        st.write("Click on a crash marker for details or elsewhere to clear selection.")
+
+if 'clustered_df' in st.session_state and st.session_state['clustered_df'] is not None and st.session_state['clustered_df'].empty:
+    st.warning("No clusters found. Adjust parameters and try again.")
+elif not st.session_state.get('run_clustering', False):
+    st.write("Select severities and parameters, then click 'Run Clustering' to display results.")
